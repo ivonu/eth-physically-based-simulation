baad0d9f7ba6357becb1e851e3d38447a3ebc01f
Michael-Rabinovich <michaelrabinovich27@gmail.com>
2014-09-25 15:00:30 +0200

    midpoint and analytic

--- exercise-1/code/Exercise.cpp
+++ exercise-1/code/Exercise.cpp
@@ -6,24 +6,44 @@ using namespace std;
 
 // gravitational acceleration (9.81)
 static const double g = 9.81f;
-int CalculateForce(double p1, double p2, double length, double stiffness);
+double CalculateForce(double p1, double p2, double length, double stiffness);
 
 // Exercise 1
 // hanging mass point
-void AdvanceTimeStep1(double stiffness, double mass, double damping, double length, double time_step, int method, double p1, double v1, double& p2, double& v2)
+void AdvanceTimeStep1(double stiffness, double mass, double damping, double length, double time, int method, double p1, double v1, double& p2, double& v2)
 {
-	int force = CalculateForce(p1, p2, length, stiffness);
+	double force = CalculateForce(p1, p2, length, stiffness);
+	double initial_a = (force - damping*v2) / mass;
 	switch (method) {
 	case Scene::Method::EULER:
-		p2 = p2 + time_step*v2;
-		v2 = v2 + time_step* ((force - damping*v2) / mass);
+		p2 = p2 + time*v2;
+		v2 = v2 + time* initial_a;
 		break;
+	case Scene::Method::MIDPOINT:
+	{
+		// location
+		double mid_p = p2 + (time / 2) * v2;
+		double mid_p_v = v2 + (time / 2) * initial_a;
+		p2 = p2 + time*mid_p_v;
+		// speed
+		double mid_p_a = (CalculateForce(p1, mid_p, length, stiffness) - damping*mid_p_v) / mass;
+		v2 = v2 + time * mid_p_a;
+		break;
+	}
+	case Scene::Method::ANALYTIC: {
+		double alpha = -damping / (2 * mass); double beta = sqrt(4 * stiffness*mass - damping*damping) / (2 * mass);
+		double c1 = (mass*g) / stiffness;
+		double c2 = (-alpha / beta) * c1;
+		p2 = c1*exp(alpha*time)*cos(beta*time) + c2*exp(alpha*time)*sin(beta*time) - length - (mass*g) / stiffness;
+		break;
+	}
+		
 	default:
 		break;
 	}
 }
 
-int CalculateForce(double p1, double p2, double length, double stiffness) {
+double CalculateForce(double p1, double p2, double length, double stiffness) {
 	return stiffness*((p1-p2)-length) - g;
 }
 

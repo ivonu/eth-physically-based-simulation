95b3948ea32435ce099425fed2b1770551975383
Michael-Rabinovich <michaelrabinovich27@gmail.com>
2014-09-29 12:04:35 +0200

    Added symplectic

--- exercise-1/code/Exercise.cpp
+++ exercise-1/code/Exercise.cpp
@@ -41,6 +41,19 @@ void AdvanceTimeStep1(double stiffness, double mass, double damping, double leng
 			p2 = c1*exp(alpha*time)*cos(beta*time) + c2*exp(alpha*time)*sin(beta*time) - length - (mass*g) / stiffness;
 			break;
 		}
+
+		// the parameter's name is symplectic euler so we'll chose that one
+		case Scene::LEAP_FROG: {
+			v2 = v2 + time* initial_a;
+			p2 = p2 + time*v2;
+			break;
+		}
+		case Scene::BACK_EULER: {
+			// see bottom of page for eq doc
+			v2 = (v2 + (time / mass) * (force - damping*v2)) / (1 - (time / mass) *(time*force + damping ) );
+			p2 = p2 + time*v2;
+			break;
+		}
 		
 		default:
 		break;
@@ -48,7 +61,7 @@ void AdvanceTimeStep1(double stiffness, double mass, double damping, double leng
 }
 
 double CalculateForce(double p1, double p2, double length, double stiffness) {
-	return -stiffness*((p2-p1)-length) - g;
+	return stiffness*((p1-p2)-length) - g;
 }
 
 Vec2 CalculateForce2D(Vec2& p1, Vec2& p2, double L, double stiffness) {
@@ -88,13 +101,23 @@ void AdvanceTimeStep3(double k, double m, double d, double L, double dt,
 	Vec2 initial_a2 = (1/m) * (force_2);
 	Vec2 initial_a3 = (1 / m) * (force_3);
 
-	// Euler
-	p1 = p1 + dt*v1;
-	v1 = v1 + dt*initial_a1;
-
-	p2 = p2 + dt*v2;
-	v2 = v2 + dt*initial_a2;
-
-	p3 = p3 + dt*v3;
-	v3 = v3 + dt*initial_a3;
+	// Symplectic Euler
+	v1 = v1 + dt*initial_a1;  p1 = p1 + dt*v1;
+	v2 = v2 + dt*initial_a2;  p2 = p2 + dt*v2;
+	v3 = v3 + dt*initial_a3;  p3 = p3 + dt*v3;
 }
+
+
+/*
+backward euler 1-d
+deltaX = X_new - X;
+deltaV = V_new - V;
+F_n = F(t) + SpringF*(deltaX)+dampingF*(deltaV)
+V_new = V + (t*F_n) / M;
+X_new = X + t*V_n;
+
+
+v_n = v + (t / M) * (F + t*SpringF*V_n + dampingF*(V_n - V));
+v_n *(1 - ((t / M) *[t*Spring + dampingF])) = v + (t / M) *(F - dampinfF*V);
+v_n = (v + (t / M) * (F - dampingF*V)) / (1 - ((t / M) *[t*Spring + dampingF]))
+*/
